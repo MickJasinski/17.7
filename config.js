f0438303ed39267340ca6ff7fab21c3f94ed389b
Mick Jasinski <m.jasinski@minimalcraft.net>
2018-03-21 12:44:35 +0000

    Google login

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '777802267662-srgo9khu954v5s6nrifknb6e8mjdk08o.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '-R5_JKAFKEDTlsXojlPbqvei',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
